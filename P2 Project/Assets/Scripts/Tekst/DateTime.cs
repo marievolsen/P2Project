9b685dd8d3c940333e28dce95c3d789e380776d3
Adrenalina333 <115779476+Adrenalina333@users.noreply.github.com>
2023-05-10 18:53:50 +0200

    MedPaneler

--- P2 Project/Assets/Scripts/Tekst/DateTime.cs
+++ P2 Project/Assets/Scripts/Tekst/DateTime.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using System.Globalization;
 
 public class DateTime : MonoBehaviour
 {
     //Based on: https://www.youtube.com/watch?v=Z6pEAngpR9I&t=104s
 
-    public TextMeshPro dateText;
-    public TextMeshPro currentdayText;
-    public TextMeshPro currentmonthText;
-    public TextMeshPro currentyearText;
-    public TextMeshPro timeText;
+    public TextMeshProUGUI dateText;
+    public TextMeshProUGUI currentdayText;
+    public TextMeshProUGUI currentmonthText;
+    public TextMeshProUGUI currentyearText;
+    public TextMeshProUGUI timeText;
 
-    void Start()
+    void FixedUpdate()
     {
         string date = System.DateTime.UtcNow.ToLocalTime().ToString("dd-MM-yyyy");
-        string currentday = System.DateTime.UtcNow.ToLocalTime().ToString("dd");
+        string currentday = System.DateTime.UtcNow.ToLocalTime().ToString("dddd, dd.");
         string curentmonth = System.DateTime.UtcNow.ToLocalTime().ToString("MM");
         string currentyear = System.DateTime.UtcNow.ToLocalTime().ToString("yyyy");
         string time = System.DateTime.UtcNow.ToLocalTime().ToString("HH:mm");
---
+++ P2 Project/Assets/Scripts/UI/CallManager.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CallManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //intent.setData(Uri.parse("tel:+4570241207"));
+        //startActivity(intent);
+    }
+
+}
